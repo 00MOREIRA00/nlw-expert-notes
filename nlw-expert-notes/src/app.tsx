82a2ed04378400ee2fb402f17af31a2eda815e16
Roberto Neto <robertocorreamoreiraneto@gmail.com>
2024-02-22 14:22:43 -0300

    [nlw expert] - configurando tailwind

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -2,7 +2,9 @@ export function App() {
 
   return (
     <>
-      <p>Está funcionando como deveria</p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
